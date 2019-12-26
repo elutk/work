919d26a69c7f34e6f8c9fae56df72d41dab283e7
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:17:49 +0300

    Update 35.py

--- 35.py
+++ 35.py
@@ -11,7 +11,9 @@ A = int(input("Количесвто элементов в группе"))
 C = int(input("Позиция C"))
 B = int(input("Позиция B"))
 a = [random.randint(0, 100) for i in range(0, N)]
+
 print(a)
 for i in range(N):
     a[C: C + A], a[B: B + A] = a[C: C + A], a[B: B + A]
+    
 print(a)
