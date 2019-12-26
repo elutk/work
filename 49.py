37417fffe2a215bcff109f4810c068c62e149a17
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:32:41 +0300

    Update 49.py

--- 49.py
+++ 49.py
@@ -8,9 +8,12 @@
 # версия Python: 3.8
 import random
 import numpy
+
 N = int(input("Количество элементов в массиве"))
 A = [random.randint(-1, 1) for i in range(0, N)]
+
 print(A)
+
 for i in range(N):
     if A[i] == 0 and A[i-1] == 0:
         print("Два подряд нуля в ",i-1,"и",i)
