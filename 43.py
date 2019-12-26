cea45c3fd09473f2f1c84e6462dbe286a3562b67
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:29:09 +0300

    Update 43.py

--- 43.py
+++ 43.py
@@ -8,14 +8,17 @@
 # версия Python: 3.8
 import random
 import numpy as np
+
 N = int(input("Количество элементов массива"))
 A = [random.randint(-10,10) for i in range(0,N)]
 Ao = []
 Ap = []
+
 for i in range(N):
     if A[i] > 0:
         Ap.append(A[i])
     if A[i] < 0:
         Ao.append(A[i])
+        
 print("Отрицательные",Ao)
 print("Положительные",Ap)
