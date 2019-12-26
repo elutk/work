93b23c78404ee9c079d214e0e0020e7598caaad1
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:31:38 +0300

    Update 48.py

--- 48.py
+++ 48.py
@@ -8,10 +8,14 @@
 # версия Python: 3.8
 import random
 import numpy
+
 N = int(input("Количество элементов в массиве"))
 A = [random.randint(-2, 2) for i in range(0, N)]
+
 print(A)
+
 for i in range(N):
     if A[i] == 0:
         A[i] = A[i-1] + A[i-2]
+        
 print(A)
