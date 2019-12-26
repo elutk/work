895d037b1984da48dd584bb8c36e8cb8eecf5280
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:31:00 +0300

    Update 46.py

--- 46.py
+++ 46.py
@@ -8,10 +8,13 @@
 # версия Python: 3.8
 import random
 import numpy
+
 N = int(input("Количество элементов массива"))
 T = int(input("Положительное число"))
 A = [random.randint(-10, 10) for i in range(0, N)]
+
 print(A)
+
 S = 0
 for i in range(N):
     if A[i] > 0:
@@ -20,5 +23,6 @@ K = T/S
 for i in range(N):
     if A[i] > 0:
         A[i] += (A[i]*K)
+        
 print(K)
 print(A)
