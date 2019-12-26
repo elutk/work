2b8f2bf6b6e45882524f8dabb485e51c33bd27cb
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:16:03 +0300

    Update 30.py

--- 30.py
+++ 30.py
@@ -7,7 +7,9 @@
 # описание: Составьте блок-схему поиска максимального элемента в одномерном массиве.
 # версия Python: 3.8
 import random
+
 N = 10
 A = [random.randint(0, 100) for i in range(N)]
+
 print(A)
 print("Max =", max(A))
