c4ee3e00f4bba6d2c21916d4333aa121c3e607e3
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:31:17 +0300

    Update 47.py

--- 47.py
+++ 47.py
@@ -8,11 +8,15 @@
 # версия Python: 3.8
 import random
 import numpy
+
 N = int(input("Количество элементов массива"))
 B = int(input("Элемент массива 1"))
 C = int(input("Элемент массива 2"))
 A = [random.randint(-10, 10) for i in range(0, N)]
+
 print(A)
+
 C = C + 1
 del A[B:C]
+
 print(A)
