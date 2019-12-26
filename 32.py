e73335c3b75024c19d29a0bfb89876989d8cb460
elena._utkina.01 <55867884+elutk@users.noreply.github.com>
2019-12-26 21:16:22 +0300

    Update 32.py

--- 32.py
+++ 32.py
@@ -8,6 +8,7 @@
 # версия Python: 3.8
 import random
 import sys
+
 n = 25
 a = [random.randint(0, 100) for i in range(0,n)]
 print(a)
@@ -17,4 +18,5 @@ else:
     end = n-1
 for i in range(0, end-1, 2):
     a[i], a[i + 1] = a[i + 1], a[i]
+    
 print(a)
